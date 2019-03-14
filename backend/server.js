4c83a575036739eda3d4d50e285e895eed549184
nramabad <nramabad@gmail.com>
2019-03-13 21:17:28 -0700

    Fix api use

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const pwned = require("./routes/api/pwned");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/weather", weather);
+app.use("/api/pwned", pwned);
 
 const staticFiles = express.static(path.join(__dirname, "../../client/build"));
 app.use(staticFiles);
@@ -26,11 +28,6 @@ app.get("/", (req, res) => {
   res.send("HAI");
     // res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
-
-
-
-app.use("/api/weather", weather);
-app.use("/api/pwned", pwned);
 
 // console.log that your server is up and running
 const port = process.env.PORT || 5000;
